1305f71f23a4f3743270560fd8f38a8b05921032
sumair-khan21 <johnalex21210@gmail.com>
2022-11-26 16:33:44 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,20 @@
+// var dt = new Date();
+// var a = dt.getDate();
+// var a = dt.getHours();
+// console.log(dt);
+// var a = "December"
+// alert(a);
+// var abc = "Sat";
+// alert(abc);
+// var day = "its a fun day";
+// var funday = true;
+
+// if (day == saturday){
+//     console.log("Allow");
+// }else {
+//     if (funday == true){
+//         console.log("Allow On Student Card");
+//     } else {
+//         console.log("Not Allow");
+//     }
+// }
